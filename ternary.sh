c309a4a1ea8dea7fd034c136047146cd8f39db3b
ravi <ravi@vytec.com>
2023-11-07 09:35:39 +0530

    added

--- ternary.sh
+++ ternary.sh
@@ -1,3 +1,12 @@
-a=3; b=2
+if [ $# -ne 2 ]
+  then
+    echo " Required arguments not supplied"
+    exit
+fi
+
+a=$1; b=$2
+echo "first param: $a"
+echo "2nd param: $b"
+
 z=$((a > b ? a : b))
-echo "$z"
+echo "$z" >> /tmp/a.log
